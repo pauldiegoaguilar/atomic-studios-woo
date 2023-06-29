ee55137a27478f5798296971de2dbffca3d8e9f5
pauldiegoaguilar <diego.paul.et.n26@gmail.com>
2023-06-29 06:08:17 -0300

    cambios pequeños

--- modelos/funcion-reg.php
+++ modelos/funcion-reg.php
@@ -1,19 +1,20 @@
 <?php
-include "../includes/config.php";
-if(!empty($_POST['regi'])){
-    if (empty($_POST['nomR']) or empty($_POST['emailR']) or empty($_POST['passR'])) {
-        echo 'campos vacios';
-    }else{
-        $nombre = $_POST["nomR"];
-        $email = $_POST["emailR"];
-        $contra = $_POST["passR"];
-        $sql = "INSERT INTO usuarios (id, nombre, email, clave, genero, fotoPerfil, monedas, descripcion, flota_id, fecha_alta, fecha_baja, victorias) VALUES (NULL, '$nombre', '$email', '$contra', 'indefinido', NULL, 0, '...', 1, NOW(), NULL, 0 ) ";
-        
-        if (mysqli_query($conn, $sql)){
-            header('Location: ../main.php?section=terminos-de-servicio');
+    include "../includes/config.php";
+
+    if(!empty($_POST['regi'])){
+        if (empty($_POST['nomR']) or empty($_POST['emailR']) or empty($_POST['passR'])) {
+            echo 'campos vacios';
         }else{
-            echo'gilipollas';
+            $nombre = $_POST["nomR"];
+            $email = $_POST["emailR"];
+            $contra = $_POST["passR"];
+            $sql = "INSERT INTO usuarios (id, nombre, email, clave, genero, fotoPerfil, monedas, descripcion, flota_id, fecha_alta, fecha_baja, victorias) VALUES (NULL, '$nombre', '$email', '$contra', 'indefinido', NULL, 0, '...', 1, NOW(), NULL, 0 ) ";
+            
+            if (mysqli_query($conn, $sql)){
+                header('Location: ../main.php?section=war-over-ocean');
+            }else{
+                echo 'error de consulta';
+            }
         }
     }
-}
 ?>
