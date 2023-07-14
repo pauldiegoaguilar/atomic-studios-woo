f2eeaf59cad2a7e46e318b317256dd379c62054f
agusetn26 <augusto.etn26@gmail.com>
2023-07-14 16:41:21 -0300

    agrego esto

--- views/desarrolladores.php
+++ views/desarrolladores.php
@@ -1,46 +1,46 @@
 <div class="text-start" style="margin-top: 4%; margin-bottom: 5%; margin-left: 7.5%; margin-right: 7.5%;">
     <h2 class="text-white display-6">Acerca de Atomic Studios</h2>
     <p class="paragraph" style="color: #d3d3d3;">
-        Lorem ipsum dolor sit amet consectetur adipiscing elit, rutrum magna felis taciti dapibus conubia misociis, ac pretium natoque semper quam imperdiet. Cubilia magnis eros nam ante conubia nostra morbivarius tempus mattis primis, quam auctor porttitor sociosqu vestibulum leo vitae ligula sociiselementum, tellus eget suscipit gravida egestas sagittis faucibus congue placerat mollis. Venenatis nibhdui commodo habitasse fermentum sodales id, viverra taciti inceptos quisque neque at facilisis, facilisiphasellus condimentum mollis nulla dictumst
+        Atomic Studios es una reconocida empresa en la industria de los videojuegos, especializada en el desarrollo y la publicación de emocionantes juegos para diversas plataformas. Su juego estrella es "War Over OCEAN", un título que sumerge a los jugadores en la intensidad de las batallas navales.
     </p>
-    <h2 class="text-white display-6">¿Lorem ipsum dolor sit amet consectetur adipiscing elit, rutrum magna?</h2>
+    <h2 class="text-white display-6">¿De que se trata el juego "War Over Ocean"?</h2>
     <p class="paragraph" style="color: #d3d3d3;">
-        Felis taciti dapibus conubia mi sociis, ac pretium natoque semper quam imperdiet. Cubilia magnis erosnam ante conubia nostra morbi varius tempus mattis primis, quam auctor porttitor sociosqu vestibulumleo vitae ligula sociis elementum, tellus eget suscipit gravida egestas sagittis faucibus congue placeratmollis. Venenatis nibh dui commodo habitasse fermentum sodales id, viverra taciti inceptos quisqueneque at facilisis, facilisi phasellus condimentum mollis nulla dictumst.
+        En "War Over Ocean", los jugadores asumen el papel de comandantes de flotas y se enfrentan en una competencia estratégica y táctica en los océanos del mundo. El juego ofrece una experiencia realista de batalla naval, con gráficos impresionantes y detalles meticulosos de los barcos y el entorno marítimo. <br> Los jugadores pueden elegir entre una amplia variedad de buques de guerra históricos, desde acorazados y cruceros hasta submarinos y destructores. Cada barco tiene características únicas, como velocidad, armamento y resistencia, lo que permite a los jugadores planificar cuidadosamente sus estrategias y tácticas para derrotar a sus oponentes.
     </p>
-    <h2 class="text-white display-6">¿Lorem ipsum dolor sit amet consectetur adipiscing elit?</h2>
+    <h2 class="text-white display-6">¿Que tipos de modos hay?</h2>
     <p class="paragraph" style="color: #d3d3d3;">
-        Felis taciti dapibus conubia mi sociis, ac pretium natoque semper quam imperdiet. Cubilia magnis erosnam ante conubia nostra morbi varius tempus mattis primis.
+        "Naval Warfare: Battleships of Steel" ofrece múltiples modos de juego, incluyendo campañas en solitario, partidas multijugador en línea y desafíos cooperativos. Los jugadores pueden enfrentarse a la IA en batallas épicas o competir contra otros jugadores en intensos combates navales en línea. <br> Atomic Studios se enorgullece de proporcionar a los jugadores una experiencia inmersiva y auténtica de batalla naval. Han invertido tiempo y esfuerzo en investigar y recrear meticulosamente los barcos, las armas y las estrategias utilizadas en la vida real para brindar una experiencia de juego convincente y emocionante.
     </p>
-    <h2 class="text-white display-6">¿Lorem ipsum dolor sit amet consectetur adipiscing elit, rutrum magna?</h2>
+    <h2 class="text-white display-6">¿Quienes son los Creadores y participantes de esta empresa?</h2>
     <div class="d-flex flex-column align-items-end">
         <div class="d-flex justify-content-center" style="width: 95%; margin-top: 30px; margin-bottom: 30px;">
             <img src="img/paul.jpg" style="width: 130px; height: 130px; margin-right: 50px; border-radius: 15px;" alt="paul">
             <p class="paragraph" style="color: #d3d3d3;">
-                Paul: Felis taciti dapibus conubia mi sociis, ac pretium natoque semper quam imperdiet. Cubilia magnis eros nam ante conubia nostra morbi varius tempus mattis primis, quam auctor porttitor sociosqu vestibulum leo vitae ligula sociis elementum tel.
+                Paul: es el programador principal. Es el experto detrás de la programación y la lógica del juego. Transforma las ideas y diseños en código funcional, asegurándose de que todas las características y mecánicas del juego se implementen de manera eficiente y sin problemas.
             </p>
         </div>
         <div class="d-flex justify-content-center" style="width: 95%; margin-top: 30px; margin-bottom: 30px;">
             <img src="img/torres.png" style="width: 130px; height: 130px; margin-right: 50px; border-radius: 15px;" alt="torres">
             <p class="paragraph" style="color: #d3d3d3;">
-                Torres: Felis taciti dapibus conubia mi sociis, ac pretium natoque semper quam imperdiet. Cubilia magnis eros nam ante conubia nostra morbi varius tempus mattis primis, quam auctor porttitor sociosqu vestibulum leo vitae ligula sociis elementum tel.
+                Torres: es el diseñador principal del juego. Es el responsable de definir la visión creativa y el diseño general del juego. Trabaja en estrecha colaboración con el resto del equipo para asegurarse de que la jugabilidad sea emocionante, equilibrada y satisfactoria para los jugadores.
             </p>
         </div>
         <div class="d-flex justify-content-center" style="width: 95%; margin-top: 30px; margin-bottom: 30px;">
             <img src="img/monzon.png" style="width: 130px; height: 130px; margin-right: 50px; border-radius: 15px;" alt="monzonpro">
             <p class="paragraph" style="color: #d3d3d3;">
-                Monzonpro: Felis taciti dapibus conubia mi sociis, ac pretium natoque semper quam imperdiet. Cubilia magnis eros nam ante conubia nostra morbi varius tempus mattis primis, quam auctor porttitor sociosqu vestibulum leo vitae ligula sociis elementum tel.
+                Monzonpro: es el artista principal. Su creatividad y habilidades artísticas dan vida a los barcos, el entorno marítimo y los efectos visuales del juego. Crea modelos en 3D, texturas y animaciones impresionantes, brindando una experiencia visualmente impactante y realista.
             </p>
         </div>
         <div class="d-flex justify-content-center" style="width: 95%; margin-top: 30px; margin-bottom: 30px;">
             <img src="img/sebas.png" style="width: 130px; height: 130px; margin-right: 50px; background-color: white; border-radius: 15px;" alt="sebas">
             <p class="paragraph" style="color: #d3d3d3;">
-                Sebastian: Felis taciti dapibus conubia mi sociis, ac pretium natoque semper quam imperdiet. Cubilia magnis eros nam ante conubia nostra morbi varius tempus mattis primis, quam auctor porttitor sociosqu vestibulum leo vitae ligula sociis elementum tel.
+                Sebastian: es el responsable de la música y el sonido. Comprende la importancia de la atmósfera sonora en la inmersión de los jugadores. Compone la música de fondo, los efectos de sonido y las voces, asegurándose de que cada momento del juego esté acompañado por una experiencia auditiva cautivadora y emocionante.
             </p>
         </div>
         <div class="d-flex justify-content-center" style="width: 95%; margin-top: 30px; margin-bottom: 30px;">
             <img src="img/rodrigo.png" style="width: 130px; height: 130px; margin-right: 50px; background-color: white; border-radius: 15px;" alt="losAcostas">
             <p class="paragraph" style="color: #d3d3d3;">
-                Rodrigo: Felis taciti dapibus conubia mi sociis, ac pretium natoque semper quam imperdiet. Cubilia magnis eros nam ante conubia nostra morbi varius tempus mattis primis, quam auctor porttitor sociosqu vestibulum leo vitae ligula sociis elementum tel.
+                Rodrigo: se encarga del equilibrio del juego y la optimización. Trabaja en estrecha colaboración con el equipo de diseño y programación para ajustar la dificultad, la progresión y el rendimiento del juego. Realiza pruebas exhaustivas y realiza ajustes para garantizar que los jugadores tengan una experiencia equilibrada y fluida en todas las plataformas.
             </p>
         </div>
     </div>
